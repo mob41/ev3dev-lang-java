0f29a55d419c8efa5afab6f8e520bea4ca6e35d6
mob41 <puihuenlaw@gmail.com>
2016-04-06 23:17:25 +0800

    Enabled LargeMotor, MediumMotor driver check.

--- src/main/java/ev3dev/hardware/motors/LargeMotor.java
+++ src/main/java/ev3dev/hardware/motors/LargeMotor.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 import ev3dev.exception.InvalidMotorException;
 import ev3dev.exception.InvalidPortException;
 import ev3dev.hardware.ports.LegoPort;
+import ev3dev.io.Def;
 
 public class LargeMotor extends Motor {
 	
 	public LargeMotor(LegoPort port) throws InvalidPortException, IOException, InvalidMotorException {
 		super(port);
-		/*
 		String drivername = this.getDriverName();
-		if (!drivername.equals(PropertyDefaults.LARGE_MOTOR_DRIVER_NAME)){
-			throw new InvalidMotorException("It is not a LargeMotor (" + PropertyDefaults.LARGE_MOTOR_DRIVER_NAME + "): " + drivername);
+		if (!drivername.equals(Def.LARGE_MOTOR_DRIVER_NAME)){
+			throw new InvalidMotorException("It is not a LargeMotor (" + Def.LARGE_MOTOR_DRIVER_NAME + "): " + drivername);
 		}
-		*/
 	}
 
 }
--- src/main/java/ev3dev/hardware/motors/MediumMotor.java
+++ src/main/java/ev3dev/hardware/motors/MediumMotor.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 import ev3dev.exception.InvalidMotorException;
 import ev3dev.exception.InvalidPortException;
 import ev3dev.hardware.ports.LegoPort;
+import ev3dev.io.Def;
 
 public class MediumMotor extends Motor {
 
 	public MediumMotor(LegoPort port) throws InvalidPortException, IOException, InvalidMotorException {
 		super(port);
-		/*
 		String drivername = this.getDriverName();
-		if (!drivername.equals(PropertyDefaults.MEDIUM_MOTOR_DRIVER_NAME)){
-			throw new InvalidMotorException("It is not a MediumMotor (" + PropertyDefaults.MEDIUM_MOTOR_DRIVER_NAME + "): " + drivername);
+		if (!drivername.equals(Def.MEDIUM_MOTOR_DRIVER_NAME)){
+			throw new InvalidMotorException("It is not a MediumMotor (" + Def.MEDIUM_MOTOR_DRIVER_NAME + "): " + drivername);
 		}
-		*/
 	}
 
 }
