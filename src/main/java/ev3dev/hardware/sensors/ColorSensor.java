a8855f84688ba654e4c36809df311e02747b5e6c
mob41 <puihuenlaw@gmail.com>
2016-04-06 23:45:46 +0800

    Sensors Auto-switch mode

--- src/main/java/ev3dev/hardware/sensors/ColorSensor.java
+++ src/main/java/ev3dev/hardware/sensors/ColorSensor.java
@@ -2,6 +2,7 @@ package ev3dev.hardware.sensors;
 
 import java.io.IOException;
 
+import ev3dev.exception.InvalidModeException;
 import ev3dev.exception.InvalidPortException;
 import ev3dev.exception.InvalidSensorException;
 import ev3dev.hardware.Device;
@@ -12,6 +13,8 @@ public class ColorSensor extends Sensor {
 	
 	private Device device;
 	
+	private boolean autoSwitchMode = true;
+	
 	public ColorSensor(LegoPort port) throws IOException, InvalidPortException, InvalidSensorException {
 		super(port);
 		if (!this.getDriverName().equals(Def.COLOR_SENSOR_DRIVER_NAME)){
@@ -20,58 +23,80 @@ public class ColorSensor extends Sensor {
 		device.getPort().getAddress();
 	}
 	
-	public int getReflectedLightIntensity() throws IOException{
+	public int getReflectedLightIntensity() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_COLOR_SENSOR_REFLECTED_LIGHT_INTENSITY_REQUIRED_MODE)){
-			System.err.println("Error: You are not using a correct mode.");
-			return -1;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_COLOR_SENSOR_REFLECTED_LIGHT_INTENSITY_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_COLOR_SENSOR_REFLECTED_LIGHT_INTENSITY_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_COLOR_SENSOR_REFLECTED_LIGHT_INTENSITY_VALUE_INDEX);
 		return Integer.parseInt(str);
 	}
 	
-	public int getAmbientLightIntensity() throws IOException{
+	public int getAmbientLightIntensity() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_COLOR_SENSOR_AMBIENT_LIGHT_INTENSITY_REQUIRED_MODE)){
-			System.err.println("Error: You are not using a correct mode.");
-			return -1;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_COLOR_SENSOR_AMBIENT_LIGHT_INTENSITY_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_COLOR_SENSOR_AMBIENT_LIGHT_INTENSITY_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_COLOR_SENSOR_AMBIENT_LIGHT_INTENSITY_VALUE_INDEX);
 		return Integer.parseInt(str);
 	}
 	
-	public int getColor() throws IOException{
+	public int getColor() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_COLOR_SENSOR_COLOR_REQUIRED_MODE)){
-			System.err.println("Error: You are not using a correct mode.");
-			return -1;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_COLOR_SENSOR_COLOR_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_COLOR_SENSOR_COLOR_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_COLOR_SENSOR_COLOR_VALUE_INDEX);
 		return Integer.parseInt(str);
 	}
 	
-	public int getRGB_Red() throws IOException{
+	public int getRGB_Red() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_COLOR_SENSOR_RGB_R_REQUIRED_MODE)){
-			System.err.println("Error: You are not using a correct mode.");
-			return -1;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_COLOR_SENSOR_RGB_R_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_COLOR_SENSOR_RGB_R_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_COLOR_SENSOR_RGB_R_VALUE_INDEX);
 		return Integer.parseInt(str);
 	}
 	
-	public int getRGB_Green() throws IOException{
+	public int getRGB_Green() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_COLOR_SENSOR_RGB_G_REQUIRED_MODE)){
-			System.err.println("Error: You are not using a correct mode.");
-			return -1;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_COLOR_SENSOR_RGB_G_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_COLOR_SENSOR_RGB_G_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_COLOR_SENSOR_RGB_G_VALUE_INDEX);
 		return Integer.parseInt(str);
 	}
 	
-	public int getRGB_Blue() throws IOException{
+	public int getRGB_Blue() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_COLOR_SENSOR_RGB_B_REQUIRED_MODE)){
-			System.err.println("Error: You are not using a correct mode.");
-			return -1;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_COLOR_SENSOR_RGB_B_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_COLOR_SENSOR_RGB_B_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_COLOR_SENSOR_RGB_B_VALUE_INDEX);
 		return Integer.parseInt(str);
+	}
+	
+	public void setAutoSwitchMode(boolean autoswitch){
+		this.autoSwitchMode = autoswitch;
 	}
 
 }
--- src/main/java/ev3dev/hardware/sensors/TouchSensor.java
+++ src/main/java/ev3dev/hardware/sensors/TouchSensor.java
@@ -2,12 +2,15 @@ package ev3dev.hardware.sensors;
 
 import java.io.IOException;
 
+import ev3dev.exception.InvalidModeException;
 import ev3dev.exception.InvalidPortException;
 import ev3dev.exception.InvalidSensorException;
 import ev3dev.hardware.ports.LegoPort;
 import ev3dev.io.Def;
 
 public class TouchSensor extends Sensor {
+	
+	public boolean autoSwitchMode = true;
 	
 	public TouchSensor(LegoPort port) throws IOException, InvalidPortException, InvalidSensorException {
 		super(port);
@@ -21,13 +24,20 @@ public class TouchSensor extends Sensor {
 		}
 	}
 	
-	public boolean isPressed() throws IOException{
+	public boolean isPressed() throws IOException, InvalidModeException{
 		if (!this.getMode().equals(Def.PROPERTY_TOUCH_REQUIRED_MODE)){
-			System.err.println("ERROR: You are not using a required mode (" + Def.PROPERTY_TOUCH_REQUIRED_MODE + " for TOUCH SENSOR: " + this.getMode());
-			return false;
+			if (autoSwitchMode){
+				this.setMode(Def.PROPERTY_TOUCH_REQUIRED_MODE);
+			} else {
+				throw new InvalidModeException("[Auto-switch is off] You are not using a correct mode(" + Def.PROPERTY_TOUCH_REQUIRED_MODE + ")! Yours: " + this.getMode());
+			}
 		}
 		String str = this.getAttribute("value" + Def.PROPERTY_TOUCH_VALUE_INDEX);
 		return str.equals("1");
+	}
+	
+	public void setAutoSwitchMode(boolean autoswitch){
+		this.autoSwitchMode = autoswitch;
 	}
 
 }
