08b3eb8df84b1a1d9eee3b33678f653d39d317d8
mob41 <puihuenlaw@gmail.com>
2016-04-27 20:58:07 +0800

    Syntax fix

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,9 @@ if [ $state = "develop"]; then
 	echo --- Development Version
 	rename target/ev3dev-lang-java-$projectversion-jar-with-dependencies.jar target/ev3dev-lang-java-$projectversion-$state-B$TRAVIS_BUILD_NUMBER.jar
 	rename target/ev3dev-lang-java-$projectversion-jar-with-dependencies.jar target/ev3dev-lang-java-$projectversion-jar-with-dependencies-$state-B$TRAVIS_BUILD_NUMBER.jar
-fi
 if [ $state = "unstable"]; then
 	echo --- Unstable Version
 	rename target/ev3dev-lang-java-$projectversion.jar target/ev3dev-lang-java-$projectversion-$state.jar
-fi
 echo ----- List All 2 Start -----
 ls -R
 echo ----- List All 2 End -----
