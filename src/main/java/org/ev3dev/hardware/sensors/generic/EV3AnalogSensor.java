b12f0ff9d6d8ea24d7db81b28739e4edccf06c7d
mob41 <puihuenlaw@gmail.com>
2016-09-06 21:19:28 +0800

    Add drivers: EV3 analog sensor

---
+++ src/main/java/org/ev3dev/hardware/sensors/generic/EV3AnalogSensor.java
@@ -0,0 +1,23 @@
+package org.ev3dev.hardware.sensors.generic;
+
+import org.ev3dev.exception.EV3LibraryException;
+import org.ev3dev.exception.InvalidPortException;
+import org.ev3dev.hardware.ports.LegoPort;
+import org.ev3dev.hardware.sensors.Sensor;
+
+public class EV3AnalogSensor extends Sensor {
+
+	/**
+	 * The EV3 Analog sensor driver name
+	 */
+	public static final String DRIVER_NAME = "ev3-analog";
+	
+	public EV3AnalogSensor(LegoPort port, String typeId) throws EV3LibraryException, InvalidPortException {
+		super(port);
+		String drivername = port.getDriverName();
+		if (!drivername.equals(DRIVER_NAME + "-" + typeId)){
+			throw new EV3LibraryException("The port is not connected to a EV3 analog sensor with type id \"" + typeId + "\": " + drivername);
+		}
+	}
+
+}
